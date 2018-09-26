34efb74d51a3d76c59201cdbe0baa43e3d8b68c6
pofider <jan.blaha@hotmail.com>
2018-09-26 12:20:08 +0200

    remove sudo

--- jsreport.Binary.Linux/download.sh
+++ jsreport.Binary.Linux/download.sh
@@ -1,8 +1,8 @@
-﻿sudo apt-get install wget -y 
+﻿apt-get install wget -y 
 wget https://github.com/jsreport/jsreport/releases/download/2.2.0/jsreport-linux.tar.gz 
 tar -xzf jsreport-linux.tar.gz 
 mv jsreport jsreport.exe 
-sudo apt-get install zip -y 
+apt-get install zip -y 
 zip jsreport.zip ./jsreport.exe
 rm jsreport-linux.tar.gz
 rm license.txt
