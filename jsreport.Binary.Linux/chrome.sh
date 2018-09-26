6cca63809ac8d66388607ada160385fba609c633
pofider <jan.blaha@hotmail.com>
2018-09-26 15:56:59 +0200

    add addons

--- jsreport.Binary.Linux/chrome.sh
+++ jsreport.Binary.Linux/chrome.sh
@@ -1,10 +1,10 @@
 # chrome dependencies
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
-sudo apt-get install -y libstdc++-4.9-dev
-sudo apt-get install -y libstdc++6 libgcc
-sudo apt-get install -y libgconf-2-4
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst --no-install-recommends
+# sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+# sudo apt-get update
+# sudo apt-get install -y libstdc++-4.9-dev
+# sudo apt-get install -y libstdc++6 libgcc
+# sudo apt-get install -y libgconf-2-4
+# sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+# sudo apt-get update
+# sudo apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst --no-install-recommends
