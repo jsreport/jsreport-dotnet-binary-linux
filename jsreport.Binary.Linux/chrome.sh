36ddeb9e090d624eb73ced515b027bf9cb60b6ef
pofider <jan.blaha@hotmail.com>
2018-09-26 14:54:45 +0200

    try to add some c++ deps

--- jsreport.Binary.Linux/chrome.sh
+++ jsreport.Binary.Linux/chrome.sh
@@ -1,5 +1,8 @@
 # chrome dependencies
-sudo apt-get install -y libstdc++6
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install libstdc++-4.9-dev
+sudo apt-get install -y libstdc++6 libgcc
 sudo apt-get install -y libgconf-2-4
 sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
