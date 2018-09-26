c12aa2b945587541b23da026709dfabcc688accc
pofider <jan.blaha@hotmail.com>
2018-09-26 11:46:32 +0200

    download zip during prebuild event

---
+++ jsreport.Binary.Linux/download.sh
@@ -0,0 +1,8 @@
+﻿wget https://github.com/jsreport/jsreport/releases/download/2.2.0/jsreport-linux.tar.gz
+tar -xzf jsreport-linux.tar.gz
+mv jsreport jsreport.exe
+sudo apt-get install zip -y
+zip jsreport.zip jsreport.exe
+rm jsreport-linux.tar.gz
+rm license.txt
+rm jsreport.exe
