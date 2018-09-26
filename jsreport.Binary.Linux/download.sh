ade23cf0bf60e500dad510bd4175c2e73744046c
pofider <jan.blaha@hotmail.com>
2018-09-26 12:04:10 +0200

    fix download script

--- jsreport.Binary.Linux/download.sh
+++ jsreport.Binary.Linux/download.sh
@@ -1,8 +1,9 @@
-﻿wget https://github.com/jsreport/jsreport/releases/download/2.2.0/jsreport-linux.tar.gz
-tar -xzf jsreport-linux.tar.gz
-mv jsreport jsreport.exe
-sudo apt-get install zip -y
-zip jsreport.zip jsreport.exe
+﻿sudo apt-get install wget -y 
+wget https://github.com/jsreport/jsreport/releases/download/2.2.0/jsreport-linux.tar.gz 
+tar -xzf jsreport-linux.tar.gz 
+mv jsreport jsreport.exe 
+sudo apt-get install zip -y 
+zip jsreport.zip ./jsreport.exe
 rm jsreport-linux.tar.gz
 rm license.txt
 rm jsreport.exe
