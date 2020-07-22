2ab85a8c2feebc839467d130a925979a7a954363
pofider <jan.blaha@hotmail.com>
2020-07-22 15:10:11 +0200

    release 2.9.0

--- jsreport.Binary.Linux/download.ps1
+++ jsreport.Binary.Linux/download.ps1
@@ -1,5 +1,5 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-(new-object System.Net.WebClient).DownloadFile('https://github.com/jsreport/jsreport/releases/download/2.7.1/jsreport-linux.tar.gz','jsreport-linux.tar.gz')
+(new-object System.Net.WebClient).DownloadFile('https://github.com/jsreport/jsreport/releases/download/2.9.0/jsreport-linux.tar.gz','jsreport-linux.tar.gz')
 & cmd.exe '/C 7z x "jsreport-linux.tar.gz" -so | 7z x -aoa -si -ttar'
 ren jsreport jsreport.exe
 Compress-Archive -LiteralPath jsreport.exe  -CompressionLevel Optimal -DestinationPath jsreport.zip -Force
