c49f2b0f4c88249a684aa7b7c8edc5465483a2e4
pofider <honza.pofider@seznam.cz>
2023-03-27 08:16:16 +0200

    release 3.11.0

--- jsreport.Binary.Linux/download.sh
+++ jsreport.Binary.Linux/download.sh
@@ -1,5 +1,5 @@
 ﻿#download the binary from github and zip it as expected by c#
-wget https://github.com/jsreport/jsreport/releases/download/3.9.0/jsreport-linux.tar.gz 
+wget https://github.com/jsreport/jsreport/releases/download/3.11.0/jsreport-linux.tar.gz 
 tar -xzf jsreport-linux.tar.gz 
 mv jsreport jsreport.exe 
 zip jsreport-new.zip ./jsreport.exe
